b02d9eabc78b16e47b8def440f501b7314af87f9
howudong <tjdvy963@naver.com>
2024-03-13 22:01:17 +0900

    feat : 성공 및 메시지를 담은 Enum Class 이자 성공 정보를 반환하는 SuccessStatus 추가 #1

---
+++ src/main/java/spharos/msg/global/api/code/status/SuccessStatus.java
@@ -0,0 +1,37 @@
+package spharos.msg.global.api.code.status;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import spharos.msg.global.api.code.BaseCode;
+import spharos.msg.global.dto.ReasonDto;
+
+@Getter
+@RequiredArgsConstructor
+public enum SuccessStatus implements BaseCode {
+    LOGIN_SUCCESS(HttpStatus.ACCEPTED, "USER202", "로그인 성공"),
+
+    SIGN_UP_SUCCESS(HttpStatus.CREATED, "USER201", "회원가입 성공");
+
+    private final HttpStatus httpStatus;
+    private final String status;
+    private final String message;
+
+    @Override
+    public ReasonDto getReason() {
+        return ReasonDto.builder()
+                .message(message)
+                .status(status)
+                .build();
+    }
+
+    @Override
+    public ReasonDto getReasonHttpStatus() {
+        return ReasonDto.builder()
+                .message(message)
+                .status(status)
+                .httpStatus(httpStatus)
+                .build();
+
+    }
+}
