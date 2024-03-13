7efe68aa851ae79c89a5dd75aa82bbf1c7fa3c9e
howudong <tjdvy963@naver.com>
2024-03-13 21:55:52 +0900

    refactor : apiResponse와 동일 기능을 하는 BasicResponseDto.java 삭제 및 관련 있는 코드 리팩토링 #1

--- src/main/java/spharos/msg/domain/cart/controller/CartController.java
+++ src/main/java/spharos/msg/domain/cart/controller/CartController.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import spharos.msg.domain.cart.dto.CartRequestDto;
 import spharos.msg.domain.cart.service.CartService;
-import spharos.msg.global.dto.BasicResponseDto;
+import spharos.msg.global.api.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,45 +15,48 @@ public class CartController {
     private final CartService cartService;
 
     @PostMapping
-    public BasicResponseDto<?> addCart(
+    public ApiResponse<?> addCart(
             @RequestBody CartRequestDto cartRequestDto
     ) {
         cartService.addCart(cartRequestDto);
         return null;
     }
+
     //장바구니 전체 조회
     @GetMapping
-    public BasicResponseDto<?> getCart(
+    public ApiResponse<?> getCart(
             @AuthenticationPrincipal UserDetails userDetails
-            ) {
+    ) {
         cartService.getCart(userDetails);
         return null;
     }
 
     @PatchMapping("/{cartId}")
-    public BasicResponseDto<?> updateCart(
+    public ApiResponse<?> updateCart(
             @RequestBody CartRequestDto cartRequestDto,
             @AuthenticationPrincipal UserDetails userDetails,
             @PathVariable Long cartId
-    ){
-        cartService.updateCart(cartRequestDto,userDetails,cartId);
+    ) {
+        cartService.updateCart(cartRequestDto, userDetails, cartId);
         return null;
     }
+
     @DeleteMapping("/{cartId}")
-    public BasicResponseDto<?> deleteCart(
+    public ApiResponse<?> deleteCart(
             @AuthenticationPrincipal UserDetails userDetails,
             @PathVariable Long cartId
-    ){
-        cartService.deleteCart(userDetails,cartId);
+    ) {
+        cartService.deleteCart(userDetails, cartId);
         return null;
     }
+
     //장바구니 상품 옵션 조회
     @GetMapping("/option/{productId}")
-    public BasicResponseDto<?> getCartOption(
+    public ApiResponse<?> getCartOption(
             @AuthenticationPrincipal UserDetails userDetails,
             @PathVariable Long productId
-    ){
-        cartService.getCartOption(userDetails,productId);
+    ) {
+        cartService.getCartOption(userDetails, productId);
         return null;
     }
 }
