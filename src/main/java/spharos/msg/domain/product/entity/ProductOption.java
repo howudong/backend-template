58747e360b3e6975aa8ed0a46f6ba8d0e26da54d
howudong <tjdvy963@naver.com>
2024-03-15 16:34:33 +0900

    feat : Option 테이블 매핑 관계 추가 #10

--- src/main/java/spharos/msg/domain/product/entity/ProductOption.java
+++ src/main/java/spharos/msg/domain/product/entity/ProductOption.java
@@ -3,6 +3,9 @@ package spharos.msg.domain.product.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import spharos.msg.domain.product.entity.option.OptionColor;
+import spharos.msg.domain.product.entity.option.OptionEtc;
+import spharos.msg.domain.product.entity.option.OptionSize;
 import spharos.msg.global.entity.BaseEntity;
 
 @Entity
@@ -21,4 +24,16 @@ public class ProductOption extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_size_id")
+    private OptionSize optionSize;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_color_id")
+    private OptionColor optionColor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "option_etc_id")
+    private OptionEtc optionEtc;
 }
