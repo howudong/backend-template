bd6b7e8ffdff60ab97eef80da0cb95c6faf435b1
howudong <tjdvy963@naver.com>
2024-03-18 14:52:13 +0900

    refactor : categoryName 컬럼을 CategoryProduct로 위치 이동 #25

--- src/main/java/spharos/msg/domain/category/entity/Category.java
+++ src/main/java/spharos/msg/domain/category/entity/Category.java
@@ -1,22 +1,28 @@
 package spharos.msg.domain.category.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 import lombok.Getter;
 import spharos.msg.global.entity.BaseEntity;
-
-import java.util.List;
 
 @Entity
 @Getter
 public class Category extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "category_id")
     private Long id;
-
-    @NotNull
-    private String categoryName;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_parent_id")
@@ -24,4 +30,12 @@ public class Category extends BaseEntity {
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child;
+
+    @NotNull
+    private String categoryName;
+
+    @NotNull
+    @Max(3)
+    @Column(columnDefinition = "integer default 0")
+    private Integer productCategoryLevel;
 }
