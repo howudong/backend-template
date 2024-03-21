ea3326377ec0e506d2347fac415d0ece4b8734ed
howudong <tjdvy963@naver.com>
2024-03-21 11:13:18 +0900

    feat : swagger Bearer 방식의 Authorized 추가

--- src/main/java/spharos/msg/global/config/SwaggerConfig.java
+++ src/main/java/spharos/msg/global/config/SwaggerConfig.java
@@ -3,24 +3,39 @@ package spharos.msg.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String BEARER_TOKEN = "Bearer ";
+    private static final String BEARER_SCHEME = "bearer";
+    private static final String BEARER_FORMAT = "JWT";
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+            .components(new Components())
+            .addSecurityItem(new SecurityRequirement().addList(BEARER_TOKEN))
+            .components(new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes(BEARER_TOKEN, new SecurityScheme()
+                    .name(BEARER_TOKEN)
+                    .type(Type.HTTP)
+                    .scheme(BEARER_SCHEME)
+                    .bearerFormat(BEARER_FORMAT)))
+            .info(apiInfo());
     }
+
 
     private Info apiInfo() {
         return new Info()
-                .title("MSG조 Springdoc 테스트")
-                .description("Springdoc을 사용한 Swagger UI 테스트")
-                .version("1.0.0");
+            .title("MSG조 Springdoc 테스트")
+            .description("Springdoc을 사용한 Swagger UI 테스트")
+            .version("1.0.0");
     }
 }
 
