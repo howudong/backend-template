c44467943899ead39528bb5523272b26a3ac0fc1
howudong <tjdvy963@naver.com>
2024-03-21 15:45:00 +0900

    ci : 필요 없는 어노테이션 주석처리해서 테스트 실패 방지 #35

--- src/test/java/spharos/msg/MsgApplicationTests.java
+++ src/test/java/spharos/msg/MsgApplicationTests.java
@@ -1,13 +1,12 @@
 package spharos.msg;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MsgApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
