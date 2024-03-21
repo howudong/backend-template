b9329f040163872b9308bdece665a70896383a2c
howudong <tjdvy963@naver.com>
2024-03-21 11:26:40 +0900

    refactor : 이미 SpringDataJpa에서 제공하는 메서드이기 때문에 제거 #21

--- src/main/java/spharos/msg/domain/users/repository/AddressRepository.java
+++ src/main/java/spharos/msg/domain/users/repository/AddressRepository.java
@@ -1,11 +1,8 @@
 package spharos.msg.domain.users.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spharos.msg.domain.users.entity.Address;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
-    @Override
-    Optional<Address> findById(Long addressId);
 }
