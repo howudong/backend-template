e4d478450085092cd4e3d55b34f3c3fe5720692b
I-HYEON <tjwn1408@naver.com>
2024-03-25 00:23:41 +0900

    refactor: responseDto 파일 삭제 및 클래스 수정, 이동#33

---
+++ src/main/java/spharos/msg/domain/product/dto/ProductResponse.java
@@ -0,0 +1,60 @@
+package spharos.msg.domain.product.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.math.BigDecimal;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Builder
+@Setter(AccessLevel.NONE)
+public class ProductResponse {
+    @Data
+    @Builder
+    public static class HomeCosmeRandomFoodDto {
+
+        private List<ProductInfo> cosmeticList;
+        private List<ProductInfo> randomList;
+        private List<ProductInfo> foodList;
+    }
+    @Data
+    @Builder
+    public static class HomeFashionDto {
+
+        private List<ProductInfo> fashionList;
+    }
+
+    @Getter
+    @Setter
+    public static class ProductInfo {
+
+        @Schema(description = "상품 id")
+        private Long productId;
+        @Schema(description = "상품 이름")
+        private String productName;
+        @Schema(description = "상품 정상가")
+        private Integer productPrice;
+        @Schema(description = "상품 이미지")
+        private String image;
+        @Schema(description = "상품 별점")
+        private Short productStar;
+        @Schema(description = "상품 할인율")
+        private BigDecimal discountRate;
+
+        @Builder
+        private ProductInfo(Long productId, String productName, Integer productPrice, String image,
+            Short productStar, BigDecimal discountRate) {
+
+            this.productId = productId;
+            this.productName = productName;
+            this.productPrice = productPrice;
+            this.image = image;
+            this.productStar = productStar;
+            this.discountRate = discountRate;
+        }
+    }
+}
--- src/main/java/spharos/msg/domain/product/service/ProductService.java
+++ src/main/java/spharos/msg/domain/product/service/ProductService.java
@@ -12,14 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import spharos.msg.domain.product.dto.ProductDetailInfoDto;
-import spharos.msg.domain.product.dto.ProductInfoDto;
-import spharos.msg.domain.product.dto.ProductResponseDto;
+import spharos.msg.domain.product.dto.ProductResponse;
 import spharos.msg.domain.product.entity.Product;
-import spharos.msg.domain.product.entity.ProductSalesInfo;
 import spharos.msg.domain.product.repository.ProductRepository;
 import java.util.List;
 import java.util.stream.Collectors;
-import spharos.msg.domain.product.repository.ProductSalesInfoRepository;
 import spharos.msg.global.api.ApiResponse;
 
 @Service
@@ -28,39 +25,37 @@ import spharos.msg.global.api.ApiResponse;
 public class ProductService {
 
     private final ProductRepository productRepository;
-    private final ProductSalesInfoRepository productSalesInfoRepository;
 
     //Home화면 상품 조회
-    public ProductResponseDto.HomeCosmeRandomFood getHomeCosmeRandomFood() {
+    public ProductResponse.HomeCosmeRandomFoodDto getHomeCosmeRandomFood() {
 
         List<Product> beautyProducts = productRepository.findProductsByCategoryName("뷰티");
         List<Product> foodProducts = productRepository.findProductsByCategoryName("신선식품");
         List<Product> randomProducts = productRepository.findRandomProducts();
 
-        //ProductInfo Dto가 담긴 리스트로 변환
-        List<ProductInfoDto> beautys = beautyProducts.stream()
+        List<ProductResponse.ProductInfo> beautys = beautyProducts.stream()
             .limit(6)
             .map(this::mapToProductInfoDto)
             .collect(Collectors.toList());
 
-        List<ProductInfoDto> randoms = randomProducts.stream()
+        List<ProductResponse.ProductInfo> randoms = randomProducts.stream()
             .limit(12)
             .map(this::mapToProductInfoDto)
             .collect(Collectors.toList());
 
-        List<ProductInfoDto> foods = foodProducts.stream()
+        List<ProductResponse.ProductInfo> foods = foodProducts.stream()
             .limit(12)
             .map(this::mapToProductInfoDto)
             .collect(Collectors.toList());
 
-        return ProductResponseDto.HomeCosmeRandomFood.builder()
+        return ProductResponse.HomeCosmeRandomFoodDto.builder()
             .cosmeticList(beautys)
             .randomList(randoms)
             .foodList(foods)
             .build();
     }
 
-    public ProductResponseDto.HomeFashion getHomeFashion(int index) {
+    public ProductResponse.HomeFashionDto getHomeFashion(int index) {
         //pageble 객체 생성
         Pageable pageable = PageRequest.of(index, 16);
         //index 기반 패션 상품들 조회
@@ -68,11 +63,11 @@ public class ProductService {
         List<Product> fashionProducts = fashionProductsPage.getContent();
 
         // 조회된 상품들 ProductInfoDto 리스트로 변환
-        List<ProductInfoDto> fashions = fashionProducts.stream()
+        List<ProductResponse.ProductInfo> fashions = fashionProducts.stream()
             .map(this::mapToProductInfoDto)
             .collect(Collectors.toList());
 
-        return ProductResponseDto.HomeFashion.builder()
+        return ProductResponse.HomeFashionDto.builder()
             .fashionList(fashions)
             .build();
     }
@@ -80,9 +75,9 @@ public class ProductService {
     @Transactional
     public ApiResponse<?> getProductDetail(Long productId) {
 
-        //id값으로 상품 조회
+        //id로 상품 조회
         Optional<Product> productOptional = productRepository.findById(productId);
-        //해당 객체가 존재하는지 확인 후, Dto 매핑
+        //해당 객체가 존재 하는지 확인 후, Dto 매핑
         if (productOptional.isPresent()) {
             Product product = productOptional.get();
 
@@ -101,9 +96,9 @@ public class ProductService {
     }
 
 
-    //Product 엔티티를 ProductInfo Dto로 매핑하는 메서드
-    private ProductInfoDto mapToProductInfoDto(Product product) {
-        return ProductInfoDto.builder()
+    //product를 ProductInfo 형식으로 매핑하는 메서드
+    private ProductResponse.ProductInfo mapToProductInfoDto(Product product) {
+        return ProductResponse.ProductInfo.builder()
             .productId(product.getId())
             .productName(product.getProductName())
             .productPrice(product.getProductPrice())
